4b0b451e5217aaa6f2a5fbc60ceebf413700b227
ricky-lhy <rickylhy277@gmail.com>
2025-04-09 01:13:18 +0800

    bug fixed for test_app.py

--- test_app.py
+++ test_app.py
@@ -4,5 +4,4 @@ def test_homepage():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
-        assert b'To-Do List' in response.data
-        
+        assert b'My To-Do App' in response.data
